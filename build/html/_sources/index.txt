5a4cda13966488063e853769ceb6da9eeb1bda32
Sérgio Vilar <sergiovilar.r@gmail.com>
2014-06-27 23:11:38 -0300

    First commit

---
+++ build/html/_sources/index.txt
@@ -0,0 +1,17 @@
+Alter Framework documentation
+===========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Alter Framework documentation"],objects:{},titleterms:{welcom:[],indic:0,framework:0,tabl:0,document:0,alter:0}})
